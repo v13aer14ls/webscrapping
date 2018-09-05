fa2a78d55e03b16ca9f1dbfc66342bed5a8c77c6
v13ar14ls <v13aer14ls@gmail.com>
2018-09-05 17:08:29 -0300

    Progredindo

---
+++ script2.py
@@ -0,0 +1,6 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = ("https://www.pythonscraping.com/exercises/exercise1.html")
+bs0bj = BeautifulSoup(html.read()):
+print(bs0bj.h1)
