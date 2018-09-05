9f9e8ce69fd4e2aced7c04be9f459004aa61f6c2
v13ar14ls <v13aer14ls@gmail.com>
2018-09-05 18:23:25 -0300

    Deu bom

--- script4.py
+++ script4.py
@@ -2,3 +2,9 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 html = urlopen("http://www.pythonscraping.com/pages/warandpeace.html")
 bs0bj = BeautifulSoup(html)
+
+#Usando find all para extrair uma lista de nomes próprios encontrados selecionando apenas o texto existente nas tags
+
+nameList = bs0bj.findAll("span", {"class":"green"})
+for name in nameList:
+    print(name.get_text())
