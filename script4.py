9bca3e90b2e52cf788fff844d2e81c2aaf17417f
v13ar14ls <v13aer14ls@gmail.com>
2018-09-05 19:02:19 -0300

    comitando

--- script4.py
+++ script4.py
@@ -3,8 +3,9 @@ from bs4 import BeautifulSoup
 html = urlopen("http://www.pythonscraping.com/pages/warandpeace.html")
 bs0bj = BeautifulSoup(html)
 
-#Usando find all para extrair uma lista de nomes próprios encontrados selecionando apenas o texto existente nas tags
+#Usando find all para extrair uma lista de nomes próprios encontrados selecionando apenas o texto existente nas tags verdeself.
 
 nameList = bs0bj.findAll("span", {"class":"green"})
 for name in nameList:
     print(name.get_text())
+#Nota, chamar o get text quase sempre por ultimo. Preservar tags o maximo possivel.
