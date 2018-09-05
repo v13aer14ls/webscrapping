6e272cd645cc7ea9c3bc9abc75fd586af72da20f
v13ar14ls <v13aer14ls@gmail.com>
2018-09-05 17:19:41 -0300

    la vamos nos

--- script2.py
+++ script2.py
@@ -1,6 +1,5 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-
 html = ("https://www.pythonscraping.com/exercises/exercise1.html")
-bs0bj = BeautifulSoup(html.read()):
+bs0bj = BeautifulSoup(html.read());
 print(bs0bj.h1)
