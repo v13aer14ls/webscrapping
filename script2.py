fb3ea8456634db2e70b062636df284a5cc81c173
v13ar14ls <v13aer14ls@gmail.com>
2018-09-05 17:24:40 -0300

    agora foi

--- script2.py
+++ script2.py
@@ -1,5 +1,5 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-html = ("https://www.pythonscraping.com/exercises/exercise1.html")
+html = urlopen("https://www.pythonscraping.com/exercises/exercise1.html")
 bs0bj = BeautifulSoup(html.read());
 print(bs0bj.h1)
